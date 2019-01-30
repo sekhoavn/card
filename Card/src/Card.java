0487e3723e6e18c22885ba0d22659975504c177d
Asidhu3 <sidhu376@OA-E200-07.shernet.sheridancollege.ca>
2019-01-29 20:52:57 -0500

    First commit for card

---
+++ Card/src/Card.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author test
+ */
+public class Card {
+
+    private String suit;
+    //Hearts,Spades,Diamonds,Clubs
+
+        private int value;
+        
+        public Card(String givenSuit,int givenValue)
+        {
+            suit=givenSuit;
+            value=givenValue;
+        }
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        if(suit.equals("Hearts")||suit.equals("Spades")||suit.equals("Diamonds")||suit.equals("Clubs"))
+        {
+        this.suit = suit;
+        }
+        else
+        {
+            System.out.println("Hello.Not valid Suit. Please check");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if(value<1 || value>13)
+        {
+            System.out.println("Not in range 1-13");
+        }
+        else
+        {
+            this.value = value;
+        }
+    }
+    
+
+    
+}
---
+++ Card/src/CardTester.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sidhu376
+ */
+public class CardTester {
+    public static void main(String[] args)
+    {
+        Card c1=new Card("Hearts",7);
+        c1.setSuit("Spades");
+        System.out.println ("New suit is "+ c1.getSuit());
+        
+    }
+}
